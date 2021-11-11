709218b23a1735a514db864433198fe54dc26371
Dmytro Hrab <dmytro.hrab@startupsoft.co>
2021-11-11 16:51:36 +0200

    Add input "Description" and change type button

--- src/components/FormExpenses.tsx
+++ src/components/FormExpenses.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import moment, { Moment } from "moment";
 
 import { Button, DatePicker, Input } from "antd";
@@ -11,6 +11,9 @@ const FormExpenses = () => {
   const [typeExpenses, setTypeExpenses] = useState("income");
   const [dateExpenses, setDateExpenses] = useState(moment().format(dateFormat));
   const [amountMoney, setAmountMoney] = useState<null | number>(null);
+  const [descriptionExpenses, setDescriptionExpenses] = useState<null | string>(
+    null
+  );
 
   const onChangeDatePicker = (date: Moment, dateString: string) => {
     setDateExpenses(dateString);
@@ -21,16 +24,26 @@ const FormExpenses = () => {
     setAmountMoney(+value);
   };
 
-  const onSubmit = () => {
+  const onChangeInputDescription = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setDescriptionExpenses(value);
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     console.log(typeExpenses);
     console.log(dateExpenses);
     console.log(amountMoney);
     setDateExpenses(moment().format(dateFormat));
     setAmountMoney(null);
+    if (descriptionExpenses) {
+      console.log(descriptionExpenses);
+      setDescriptionExpenses(null);
+    }
   };
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <SelectExpenses
         typeExpenses={typeExpenses}
         changeType={setTypeExpenses}
@@ -49,7 +62,13 @@ const FormExpenses = () => {
         placeholder="Enter amount"
         required
       />
-      <Button type="primary" onClick={onSubmit}>
+      <Input
+        type="text"
+        placeholder="Description"
+        value={descriptionExpenses}
+        onChange={onChangeInputDescription}
+      />
+      <Button type="primary" htmlType="submit">
         Send
       </Button>
     </form>
