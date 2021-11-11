bf267cda6ba6eaa3d16ca3a9d59a9a4ab954ad25
Dmytro Hrab <dmytro.hrab@startupsoft.co>
2021-11-11 16:26:32 +0200

    Add input "Amount"

--- src/components/FormExpenses.tsx
+++ src/components/FormExpenses.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import moment, { Moment } from "moment";
 
-import { Button, DatePicker } from "antd";
+import { Button, DatePicker, Input } from "antd";
 
 import SelectExpenses from "./SelectExpenses";
 
@@ -10,19 +10,27 @@ const dateFormat = "DD-MM-YYYY";
 const FormExpenses = () => {
   const [typeExpenses, setTypeExpenses] = useState("income");
   const [dateExpenses, setDateExpenses] = useState(moment().format(dateFormat));
+  const [amountMoney, setAmountMoney] = useState<null | number>(null);
 
-  const onChange = (date: Moment, dateString: string) => {
+  const onChangeDatePicker = (date: Moment, dateString: string) => {
     setDateExpenses(dateString);
+  };
+
+  const onChangeInputMoney = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setAmountMoney(+value);
   };
 
   const onSubmit = () => {
     console.log(typeExpenses);
     console.log(dateExpenses);
+    console.log(amountMoney);
     setDateExpenses(moment().format(dateFormat));
+    setAmountMoney(null);
   };
 
   return (
-    <div>
+    <form>
       <SelectExpenses
         typeExpenses={typeExpenses}
         changeType={setTypeExpenses}
@@ -31,12 +39,20 @@ const FormExpenses = () => {
         defaultValue={moment(dateExpenses, dateFormat)}
         value={moment(dateExpenses, dateFormat)}
         format={dateFormat}
-        onChange={onChange}
+        onChange={onChangeDatePicker}
+      />
+      <Input
+        type="number"
+        value={amountMoney}
+        onChange={onChangeInputMoney}
+        maxLength={25}
+        placeholder="Enter amount"
+        required
       />
       <Button type="primary" onClick={onSubmit}>
         Send
       </Button>
-    </div>
+    </form>
   );
 };
 
