517cbd7ca8f78b4ec8c1de5299b4fb6cefb89f83
Dmytro Hrab <dmytro.hrab@startupsoft.co>
2021-11-11 15:43:03 +0200

    Add calendar

--- src/components/FormExpenses.tsx
+++ src/components/FormExpenses.tsx
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
+import moment, { Moment } from "moment";
 
-import { Button } from "antd";
+import { Button, DatePicker } from "antd";
 
 import SelectExpenses from "./SelectExpenses";
 
+const dateFormat = "DD-MM-YYYY";
+
 const FormExpenses = () => {
   const [typeExpenses, setTypeExpenses] = useState("income");
+  const [dateExpenses, setDateExpenses] = useState(moment().format(dateFormat));
+
+  const onChange = (date: Moment, dateString: string) => {
+    setDateExpenses(dateString);
+  };
+
+  const onSubmit = () => {
+    console.log(typeExpenses);
+    console.log(dateExpenses);
+    setDateExpenses(moment().format(dateFormat));
+  };
 
   return (
     <div>
@@ -13,7 +27,13 @@ const FormExpenses = () => {
         typeExpenses={typeExpenses}
         changeType={setTypeExpenses}
       />
-      <Button type="primary" onClick={() => console.log(typeExpenses)}>
+      <DatePicker
+        defaultValue={moment(dateExpenses, dateFormat)}
+        value={moment(dateExpenses, dateFormat)}
+        format={dateFormat}
+        onChange={onChange}
+      />
+      <Button type="primary" onClick={onSubmit}>
         Send
       </Button>
     </div>
