833c2da3aa9d53e69190d50ac6981ec2dea7b994
Dmytro Hrab <dmytro.hrab@startupsoft.co>
2021-12-14 11:20:39 +0200

    Add statistic to page

--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { useGlobalContext } from "../store/GlobalContext";
+import { RecordType } from "Constants/types";
 
 const StatisticsPage = () => {
-  return <>OH shit</>;
+  const { recordCollection } = useGlobalContext();
+
+  const [data, setData] = useState<RecordType[]>(null);
+  const [store, setStore] = useState({
+    income: null,
+    expenses: null,
+    total: null,
+  });
+
+  useEffect(() => {
+    (async () => {
+      const fetchData = await recordCollection.getAll();
+      setData(fetchData);
+      let income = 0,
+        expenses = 0,
+        total = 0;
+      fetchData.forEach((e) => {
+        if (e.typeRecord === "Income") income += e.amountMoney;
+        else expenses += e.amountMoney;
+      });
+      total = income - expenses;
+      setStore({
+        income: income,
+        expenses: expenses,
+        total: total,
+      });
+    })();
+  }, []);
+
+  return (
+    <>
+      <div>Income:{store.income}</div>
+      <div>Expenses:{store.expenses}</div>
+      <div>Total:{store.total}</div>
+    </>
+  );
 };
 
 export default StatisticsPage;
